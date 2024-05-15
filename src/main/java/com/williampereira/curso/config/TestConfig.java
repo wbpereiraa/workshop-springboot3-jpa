969a15ced891e902166ac3a22c37a8faad1748c2
William Barbosa <wb.pereiraa@gmail.com>
2024-05-15 09:19:13 -0300

    Payment, one-to-one association

--- src/main/java/com/williampereira/curso/config/TestConfig.java
+++ src/main/java/com/williampereira/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.williampereira.curso.entities.Category;
 import com.williampereira.curso.entities.Order;
 import com.williampereira.curso.entities.OrderItem;
+import com.williampereira.curso.entities.Payment;
 import com.williampereira.curso.entities.Product;
 import com.williampereira.curso.entities.User;
 import com.williampereira.curso.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
